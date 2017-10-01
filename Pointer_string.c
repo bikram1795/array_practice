04bf154ff6cec56924eeb5eaed4e2c1c77a635a0
Bikram <bikramnath.office@gmail.com>
2017-10-01 21:59:41 +0530

    1 Oct

---
+++ Pointer_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void print(char* C)
+{
+	while (*C != '\0')
+	{
+		printf("%c", *C);
+		C++;
+	}
+	printf("\n");
+}
+int main()
+{
+	char C[20]="HELLO";
+	print(C);
+	return 0;
+}
